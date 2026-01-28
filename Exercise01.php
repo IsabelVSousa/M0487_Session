315bc631fb89f1e2f2822f354601d87fe95e8d19
Isabel Victoria Sousa Bria <ivsousa.dev@gmail.com>
2026-01-28 10:34:34 +0100

    form finished

--- Exercise01.php
+++ Exercise01.php
@@ -11,17 +11,20 @@ session_start();
 
 <body>
     <h1>Modify array saved in session</h1>
-    <form action="Exercise01_2.php" method="post">
+    <form action="Exercise01_2.php" method="POST">
+        <label for="value">Position to modify:</label>
         <select name="position" id="position">
             <option value="0">0</option>
             <option value="1">1</option>
             <option value="2">2</option>
         </select>
+        <br>
+        <br>
         <label for="value">New value:</label>
-        <input type="text" id="value" name="value" required>
-        <button type="submit">Modify</button>
-        <button type="submit">Average</button>
-        <button type="submit">Reset</button>
+        <input type="text" id="value" name="value" required><br><br>
+        <button type="submit" name="modify">Modify</button>
+        <button type="submit" name="average">Average</button>
+        <button type="reset" naem="reset">Reset</button>
     </form>
 </body>
 
