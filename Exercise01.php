1518f9f76b7dcde2a77b68e8e14936d1d48b0faa
Isabel Victoria Sousa Bria <ivsousa.dev@gmail.com>
2026-02-04 00:30:32 +0100

    implemented array, fixed mistake post

--- Exercise01.php
+++ Exercise01.php
@@ -10,18 +10,18 @@
 
     <?php
     session_start(); // Resume the session
-    if(isset ($_SESSION['number'])) {
-        $array = [10, 20, 30]; 
+    if(!isset ($_SESSION['numbers'])) { //!si no existe el array
+        $_SESSION['numbers'] = [10, 20, 30]; 
     }
 
     if ($_SERVER['REQUEST_METHOD'] === 'POST') {
         // Store the user input in the session
-        if (isset($_SESSION['modify'])) { //en el caso que haya clicado modify
-            $value = $_SESSION['modify']; //guardamos en una variable
-            $position = $_SESSION['position']; //tmb
-
+        if (isset($_POST['modify'])) { //en el caso que haya clicado modify
+            $value = $_POST['value']; //guardamos en una variable
+            $position = $_POST['position']; //tmb
             //modify position selected
-
+            //alteramos los datos guardados en el session con los datos post
+            $_SESSION['numbers'][$position] = $value;
 
         } else {
             echo "<h1>No session data found!</h1>";
@@ -29,10 +29,8 @@
     }
     ?>
 
-
-
     <h1>Modify array saved in session</h1>
-    <form action="Exercise01_2.php" method="POST">
+    <form method="POST">
         <label for="value">Position to modify:</label>
         <select name="position" id="position">
             <option value="0">0</option>
