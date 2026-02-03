c6b596dea25e7e1d6905c8094451dc897c2d8bdb
Isabel Victoria Sousa Bria <ivsousa.dev@gmail.com>
2026-02-04 00:43:27 +0100

    average added

--- Exercise01.php
+++ Exercise01.php
@@ -1,4 +1,3 @@
-
 <!DOCTYPE html>
 <html lang="en">
 
@@ -10,22 +9,40 @@
 
     <?php
     session_start(); // Resume the session
-    if(!isset ($_SESSION['numbers'])) { //!si no existe el array
-        $_SESSION['numbers'] = [10, 20, 30]; 
+    if (!isset($_SESSION['numbers'])) { //!si no existe el array
+        $_SESSION['numbers'] = [10, 20, 30];
     }
 
     if ($_SERVER['REQUEST_METHOD'] === 'POST') {
         // Store the user input in the session
         if (isset($_POST['modify'])) { //en el caso que haya clicado modify
+
             $value = $_POST['value']; //guardamos en una variable
             $position = $_POST['position']; //tmb
+
             //modify position selected
             //alteramos los datos guardados en el session con los datos post
             $_SESSION['numbers'][$position] = $value;
 
-        } else {
-            echo "<h1>No session data found!</h1>";
-        }
+        } 
+        // else {
+        //     echo "<h1>No session data found!</h1>";
+        // }
+    }
+
+    if ($_SERVER['REQUEST_METHOD'] === 'POST') {
+        // Store the user input in the session
+        if (isset($_POST['average'])) { //en el caso que haya clicado average
+           
+            //sumar y contar el array
+            $total = array_sum($_SESSION['numbers']);
+            $count = count($_SESSION['numbers']);
+
+            $average = number_format($total/$count, 2);
+        } 
+        // else {
+        //     echo "<h1>No session data found!</h1>";
+        // }
     }
     ?>
 
@@ -38,14 +55,17 @@
             <option value="2">2</option>
         </select>
         <br>
+
         <br>
         <label for="value">New value:</label>
-        <input type="number" id="value" name="value" required><br><br>
+        <input type="number" id="value" name="value"><br><br>
         <button type="submit" name="modify">Modify</button>
         <button type="submit" name="average">Average</button>
         <button type="reset" naem="reset">Reset</button>
         <!-- puede que los button sean input -->
+
     </form>
+
     <p>Current Array: <?php echo implode(separator: ", ", array: $_SESSION['numbers']); ?></p>
     <?php if (isset($average)) {
         echo "<p>Average: $average </p>";
